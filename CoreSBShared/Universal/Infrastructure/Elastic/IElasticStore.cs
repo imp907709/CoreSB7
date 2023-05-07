747eb69329cdcfa9f5ecda97bab49de6d0406ab6
imp907709 <imp907709@gmail.com>
2023-05-07 05:02:11 +0300

    TMPL elastic store, not tested

---
+++ CoreSBShared/Universal/Infrastructure/Elastic/IElasticStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using CoreSBShared.Universal.Infrastructure.Interfaces;
+
+namespace CoreSBShared.Universal.Infrastructure.Elastic
+{
+    public interface IElasticStore
+    {
+        void SetIndex(string indexName);
+        Task<T> GetByIdAsync<T, TKey>(TKey id) where T : class, IEntity<TKey>;
+        Task<T?> GetByIdAsync<T>(string id) where T : class, IEntityStringId;
+        Task<T> AddAsync<T>(T item) where T : class;
+        Task<IEnumerable<T>> AddManyAsync<T>(IEnumerable<T> items) where T : class;
+
+        Task<IEnumerable<T>> GetByFilterAsync<T>(Expression<Func<T, bool>> expression) 
+            where T : class, IEntityStringId;
+
+        Task<T> UpdateAsync<T>(T item) where T : class;
+        Task<bool> DeleteAsync<T>(T item) where T : class, IEntityStringId;
+        Task<bool> DeleteManyAsync<T>(IEnumerable<T> items) where T : class, IEntityStringId;
+        void CreateDB();
+        void DropDB();
+    }
+}
